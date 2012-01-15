5a0547749cfe951a3a438c7648713d525e03c77e
ian <ithunt0@gmail.com>
2012-01-14 19:37:15 -0500

    Stubbed & added test class

---
+++ src/HPListTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+/**
+ * @author ian hunt
+ * @date 1/14/12
+ */
+public class HPListTest {
+
+    @Test
+    public void testInsert() {
+
+
+
+    }
+
+    @Test
+    public void testFind() {
+
+    }
+
+
+}
